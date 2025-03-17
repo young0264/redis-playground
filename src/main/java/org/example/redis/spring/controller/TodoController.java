8aab4d742f6ce05545de75bc7c107a6a6877b5bc
남의영 <ny2485@naver.com>
2025-03-17 21:42:39 +0900

    feat: add Redis-based todo exam

---
+++ src/main/java/org/example/redis/spring/controller/TodoController.java
@@ -0,0 +1,31 @@
+package org.example.redis.spring.controller;
+
+import org.example.redis.spring.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public void addTodo(@RequestParam String task) {
+        todoService.addTodo(task);
+    }
+
+    @GetMapping
+    public List<String> getTodos() {
+        return todoService.getTodos();
+    }
+
+    @DeleteMapping
+    public String removeTodo() {
+        return todoService.removeTodo();
+    }
+}
