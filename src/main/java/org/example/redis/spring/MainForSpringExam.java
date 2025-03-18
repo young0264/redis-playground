e5e1e6e086be5d0acff16ba2a69de7c45e706524
남의영 <ny2485@naver.com>
2025-03-18 22:13:52 +0900

    feat: remove @SpringBootApplication and update Redis config

--- src/main/java/org/example/redis/spring/MainForSpringExam.java
+++ src/main/java/org/example/redis/spring/MainForSpringExam.java
@@ -1,12 +1,9 @@
 package org.example.redis.spring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class MainForSpringExam {
     public static void main(String[] args) {
         SpringApplication.run(MainForSpringExam.class, args);
-
     }
 }
